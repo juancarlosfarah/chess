bb8650024e7b91a47316e5592e9f7f321c1dffe1
Juan Carlos Farah <farah.juancarlos@gmail.com>
2014-12-03 01:24:49 +0000

    Added tests.

--- Test.cpp
+++ Test.cpp
@@ -27,9 +27,54 @@ int main() {
     // Test printing initialised board.
     cb.print();
 
-    // Test moving Pawn E2 to E3.
-    cb.submitMove("E2","E3");
+    // ==========
+    // Test Moves
+    // ==========
+    bool isValid;
+
+    // Test moving Pawn E2 to E4.
+    isValid = cb.submitMove("E2","E4");
+    assert(isValid == true);
     cb.print();
+
+    isValid = cb.submitMove("F1","C4");
+    assert(isValid == true);
+    cb.print();
+
+    isValid = cb.submitMove("C4","C5");
+    assert(isValid == false);
+
+    isValid = cb.submitMove("E1","E2");
+    cb.print();
+    assert(isValid == true);
+
+    isValid = cb.submitMove("E2","F3");
+    cb.print();
+    assert(isValid == true);
+
+    isValid = cb.submitMove("F3","F5");
+    assert(isValid == false);
+
+    isValid = cb.submitMove("F3","F4");
+    cb.print();
+    assert(isValid == true);
+
+    isValid = cb.submitMove("D1","H5");
+    cb.print();
+    assert(isValid == true);
+
+    isValid = cb.submitMove("H5","E3");
+    assert(isValid == false);
+
+    isValid = cb.submitMove("F4","E4");
+    assert(isValid == false);
+
+    isValid = cb.submitMove("B1","A5");
+    assert(isValid == false);
+
+    isValid = cb.submitMove("B1","A3");
+    cb.print();
+    assert(isValid == true);
 
     return 0;
 }
