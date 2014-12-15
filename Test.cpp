8e49afb442820652099028ca5280629a3cca6077
Juan Carlos Farah <farah.juancarlos@gmail.com>
2014-12-15 17:02:17 +0000

    Updated tests.

--- Test.cpp
+++ Test.cpp
@@ -101,18 +101,15 @@ int main() {
     cout << "Testing moves..." << endl;
 
     // Test moving White Pawn E2 to E4.
-    isValid = cb.submitMove("E2","E4");
-    assert(isValid == true);
+    cb.submitMove("E2","E4");
     cb.print();
 
     // Test moving Black Pawn E7 to E6.
-    isValid = cb.submitMove("E7","E6");
-    assert(isValid == true);
+    cb.submitMove("E7","E6");
     cb.print();
 
     // Test moving White Bishop F1 to C4.
-    isValid = cb.submitMove("F1","C4");
-    assert(isValid == true);
+    cb.submitMove("F1","C4");
     cb.print();
 
     // Test that Black's KingTracker is unchanged
@@ -121,178 +118,148 @@ int main() {
 
     // Test moving White Bishop back to F1.
     // Should fail because out of turn play.
-    isValid = cb.submitMove("C4","F1");
-    assert(isValid == false);
+    cb.submitMove("C4","F1");
 
     // Test moving Black Pawn A7 to B6.
     // Should fail because pawns can only move to
     // diagonally adjacent square when attacking.
-    isValid = cb.submitMove("A7","B6");
-    assert(isValid == false);
+    cb.submitMove("A7","B6");
 
     // Test moving Black Knight B8 to C6.
-    isValid = cb.submitMove("B8","C6");
-    assert(isValid == true);
-    cb.print();
+    cb.submitMove("B8","C6");
 
     // Test moving White Bishop C4 to C5.
     // Should fail because Bishop can't move vertically.
-    isValid = cb.submitMove("C4","C5");
-    assert(isValid == false);
+    cb.submitMove("C4","C5");
 
     // Test moving White King E1 to E2.
-    isValid = cb.submitMove("E1","E2");
+    cb.submitMove("E1","E2");
     cb.print();
-    assert(isValid == true);
 
     // Test that White's KingTracker is updated
     ChessSquare wkt = cb.getKingSquare(White);
     assert(wkt == ChessSquare("E2"));
 
     // Test moving Black Bishop F8 to A3.
-    isValid = cb.submitMove("F8","A3");
+    cb.submitMove("F8","A3");
     cb.print();
-    assert(isValid == true);
 
     // Test moving White Queen D1 to C1.
     // Should fail because there is a White piece in C1.
-    isValid = cb.submitMove("D1","C1");
-    assert(isValid == false);
+    cb.submitMove("D1","C1");
 
     // Test moving White Queen D1 to H5.
     // Should fail because the White King is in the way.
-    isValid = cb.submitMove("D1","H5");
-    assert(isValid == false);
+    cb.submitMove("D1","H5");
 
     // Test moving White Queen D1 to E1.
-    isValid = cb.submitMove("D1","E1");
+    cb.submitMove("D1","E1");
     cb.print();
-    assert(isValid == true);
 
     // Test moving Black Queen D8 to E7.
-    isValid = cb.submitMove("D8","E7");
+    cb.submitMove("D8","E7");
     cb.print();
-    assert(isValid == true);
 
     // Test moving White Queen H5 to E3.
     // Should fail because of invalid move dynamics.
-    isValid = cb.submitMove("H5","E3");
-    assert(isValid == false);
+    cb.submitMove("H5","E3");
 
     // Test moving White Pawn F2 to G3.
     // Should fail because pawns can only move to
     // diagonally adjacent square when attacking.
-    isValid = cb.submitMove("F2","G3");
-    assert(isValid == false);
+    cb.submitMove("F2","G3");
 
     // Test moving White Knight B1 to A5.
     // Should fail because of invalid move dynamics.
-    isValid = cb.submitMove("B1","A5");
-    assert(isValid == false);
+    cb.submitMove("B1","A5");
 
     // Test moving White Knight B1 to A3, taking
     // Black Bishop. Ensure Black Bishop is left
     // with a nullptr square property.
     ChessSquare squareWithBishop("A3");
     ChessPiece* bishop = cb.getBoard()[squareWithBishop];
-    isValid = cb.submitMove("B1","A3");
+    cb.submitMove("B1","A3");
     cb.print();
     assert(bishop->getSquare() == nullptr);
-    assert(isValid == true);
 
     // Test moving Black Queen E7 to A3, taking
     // White Knight. Ensure White Knight is left
     // with a nullptr square property.
     ChessSquare squareWithKnight("A3");
     ChessPiece* knight = cb.getBoard()[squareWithKnight];
-    isValid = cb.submitMove("E7","A3");
+    cb.submitMove("E7","A3");
     cb.print();
     assert(knight->getSquare() == nullptr);
-    assert(isValid == true);
 
     // Test moving White Knight G1 to H3.
-    isValid = cb.submitMove("G1","H3");
+    cb.submitMove("G1","H3");
     cb.print();
-    assert(isValid == true);
 
     // Test moving Black Queen A3 to A6.
-    isValid = cb.submitMove("A3","A6");
+    cb.submitMove("A3","A6");
     cb.print();
-    assert(isValid == true);
 
     // Test moving White Bishop C4 to D5.
     // Should fail because of revealed check.
-    isValid = cb.submitMove("C4","D5");
-    assert(isValid == false);
+    cb.submitMove("C4","D5");
 
     // Test moving White Bishop C4 to D3.
-    isValid = cb.submitMove("C4","D3");
+    cb.submitMove("C4","D3");
     cb.print();
-    assert(isValid == true);
 
     // Test moving Black Queen A6 to D3,
     // taking White Bishop. Ensure White
     // Bishop is left with a nullptr square.
     ChessSquare wbs("D3");
     ChessPiece* wb = cb.getBoard()[wbs];
-    isValid = cb.submitMove("A6","D3");
+    cb.submitMove("A6","D3");
     cb.print();
     assert(wb->getSquare() == nullptr);
-    assert(isValid == true);
 
     // Test moving White King E2 to F1.
     // Should fail because King still in check.
-    isValid = cb.submitMove("E2","F1");
-    assert(isValid == false);
+    cb.submitMove("E2","F1");
 
     // Test moving White King E2 to D1.
-    isValid = cb.submitMove("E2","D1");
+    cb.submitMove("E2","D1");
     cb.print();
-    assert(isValid == true);
 
     // Test moving Black Queen D3 to D4.
-    isValid = cb.submitMove("D3","D4");
+    cb.submitMove("D3","D4");
     cb.print();
-    assert(isValid == true);
 
     // Test moving White King D1 to E2.
-    isValid = cb.submitMove("D1","E2");
+    cb.submitMove("D1","E2");
     cb.print();
-    assert(isValid == true);
 
     // Test moving Black Queen D4 to D3.
-    isValid = cb.submitMove("D4","D3");
+    cb.submitMove("D4","D3");
     cb.print();
-    assert(isValid == true);
 
     // Test moving White Pawn C2 to D3,
     // taking Black Queen. Ensure Black
     // Queen is left with a nullptr square.
     ChessSquare bqs("D3");
     ChessPiece* bq = cb.getBoard()[bqs];
-    isValid = cb.submitMove("C2","D3");
+    cb.submitMove("C2","D3");
     cb.print();
     assert(bq->getSquare() == nullptr);
-    assert(isValid == true);
 
     // Test moving Black Pawn F7 to E6.
-    isValid = cb.submitMove("F7","E6");
+    cb.submitMove("F7","E6");
     cb.print();
-    assert(isValid == false);
 
     // Test moving Black Rook H8 to H1.
     // This ensures that pieces cannot
     // wrap around the board.
-    isValid = cb.submitMove("H8","H1");
+    cb.submitMove("H8","H1");
     cb.print();
-    assert(isValid == false);
 
     // Test moving Black Bishop C8 to H3.
     // This ensures that pieces cannot
     // wrap around the board.
-    isValid = cb.submitMove("C8","H3");
+    cb.submitMove("C8","H3");
     cb.print();
-    assert(isValid == false);
 
     // Reset Board
     cb.resetBoard();
@@ -429,8 +396,7 @@ int main() {
 
     // This move should not be allowed
     // as the game is now over.
-    isValid = cb.submitMove("C7","C5");
-    assert(isValid == false);
+    cb.submitMove("C7","C5");
 
     // Testing Revealed Check
     // ======================
